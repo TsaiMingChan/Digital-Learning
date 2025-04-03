0b72bab4e0b78ba4b88526e95f67323a5b95e8dd
Ema <147024078+TsaiMingChan@users.noreply.github.com>
2025-04-03 14:29:41 +0800

    Update _4bit_CLA.v

--- Homework/_4bit_CLA/_4bit_CLA.v
+++ Homework/_4bit_CLA/_4bit_CLA.v
@@ -1,29 +1,27 @@
 module _4bit_CLA(A, B, Cin, S, Cout);
 
-  // declare the output
+  // Declare the output
   output [3:0] S;
   output       Cout;
   
-  // declare the input
+  // Declare the input
   input [3:0] A;
   input [3:0] B;
   input       Cin;
 
-  // declare the wire
-  wire [3:0] G;
-  wire [3:0] P;
-  wire [3:0] Cinterval;
+  // Declare the wire
+  wire [3:0] G;  // Generate
+  wire [3:0] P;  // Propagate
+  wire [3:1] C;  // Intermediate carries
 
-  assign G = A & B;
-  assign P = A ^ B;
+  assign G = A & B;   // Generate
+  assign P = A ^ B;   // Propagate
   
-  assign Cinterval[0] = G[0] | (P[0] & Cin);
-  assign Cinterval[1] = G[1] | (P[1] & Cinterval[0]);
-  assign Cinterval[2] = G[2] | (P[2] & Cinterval[1]);
-  assign Cinterval[3] = G[3] | (P[3] & Cinterval[2]);
+  assign C[1] = G[0] | (P[0] & Cin);
+  assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & Cin);
+  assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & Cin);
+  assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & Cin);
 
-  assign Cout = Cinterval[3];
-
-  assign S = P ^ {Cinterval[2:0], Cin};
+  assign S = P ^ {C[3], C[2], C[1], Cin}; 
 
 endmodule
