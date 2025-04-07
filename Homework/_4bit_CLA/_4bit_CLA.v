fab330af5dd42aa7c07c4cb36aeaa4d5bf54d58e
Ema <147024078+TsaiMingChan@users.noreply.github.com>
2025-04-07 16:29:57 +0800

    Update _4bit_CLA.v

--- Homework/_4bit_CLA/_4bit_CLA.v
+++ Homework/_4bit_CLA/_4bit_CLA.v
@@ -10,18 +10,21 @@ module _4bit_CLA(A, B, Cin, S, Cout);
   input       Cin;
 
   // Declare the wire
-  wire [3:0] G;  // Generate
-  wire [3:0] P;  // Propagate
-  wire [3:1] C;  // Intermediate carries
+  wire [3:0] G;   // Generate
+  wire [3:0] P;   // Propagate
+  wire [3:0] P_s; // P for S
+  wire [3:1] C;   // Intermediate carries
 
   assign G = A & B;   // Generate
-  assign P = A ^ B;   // Propagate
+  assign P = A | B;   // Propagate
+
+  assign P_s = A ^ B;
   
   assign C[1] = G[0] | (P[0] & Cin);
   assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & Cin);
   assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & Cin);
   assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & Cin);
 
-  assign S = P ^ {C[3], C[2], C[1], Cin}; 
+  assign S = P_s ^ {C[3], C[2], C[1], Cin}; 
 
 endmodule
